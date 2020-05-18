445db57d2899ac4ac6e60653de5b64f19deaacf3
Mxolisi Silabela <silabelamxolisi@gmail.com>
2020-05-18 23:24:46 +0200

    Some testing again

--- Cars/src/cars/Cars.java
+++ Cars/src/cars/Cars.java
@@ -17,6 +17,7 @@ public class Cars {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("This is test");
+        System.out.println("This is test2");
     }
     
 }
