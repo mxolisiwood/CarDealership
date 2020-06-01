4589daa3e2ffe49c0599ed859e1d0cdb6cbfa715
56112755 <Wadefriedrichs42@gmail.com>
2020-06-02 00:48:13 +0200

    added some button actions

--- Cardealership/src/cardealership/account.java
+++ Cardealership/src/cardealership/account.java
@@ -84,9 +84,19 @@ public class account extends javax.swing.JFrame {
 
         jButton1.setBackground(new java.awt.Color(51, 0, 255));
         jButton1.setText("Add New Car");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton3.setBackground(new java.awt.Color(51, 255, 0));
         jButton3.setText("Sell Car");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -183,6 +193,14 @@ public class account extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // Sell button:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // Buy Button:
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
