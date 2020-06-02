c62e3a1534970c99da0820eb8a93ca0c3fa32fae
Mxolisi Silabela <silabelamxolisi@gmail.com>
2020-06-02 19:45:23 +0200

    van class fields and constructor

--- Cardealership/src/cardealership/cars/van.java
+++ Cardealership/src/cardealership/cars/van.java
@@ -9,6 +9,24 @@ package cardealership.cars;
  *
  * @author silab
  */
-public class van {
-    
+public class van extends cars{
+        public van(int Age,
+                long Milage,
+                String Brand,
+                String Model, 
+                double FuelTankSize,
+                boolean Electric,
+                String NumberPlate,
+                int PassangerNum){
+        super(Age, Milage, Brand, Model, FuelTankSize, Electric, NumberPlate);
+        
+        this.PassengerNum = PassengerNum;
+    }
+    int PassengerNum;
+    public void setPassengerNum(int value){
+        PassengerNum = value;
+    }
+    public int getPassengerNum(){
+        return PassengerNum;
+    }
 }
