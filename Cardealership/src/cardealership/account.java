9aecd75df0cb19b1a65b6f0c7c34866cfa542ad7
56112755 <Wadefriedrichs42@gmail.com>
2020-06-08 19:52:33 +0200

    Just putting forms in the middle

--- Cardealership/src/cardealership/account.java
+++ Cardealership/src/cardealership/account.java
@@ -225,6 +225,7 @@ public class account extends javax.swing.JFrame {
         getAccessibleContext().setAccessibleDescription("");
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
