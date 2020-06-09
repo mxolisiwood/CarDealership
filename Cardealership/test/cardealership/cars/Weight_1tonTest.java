9a1b6472f0d2768c8f47d8499e3aa37bf3241d7b
Mxolisi Silabela <silabelamxolisi@gmail.com>
2020-06-09 13:28:10 +0200

    Weight classes tests

---
+++ Cardealership/test/cardealership/cars/Weight_1tonTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardealership.cars;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author silab
+ */
+public class Weight_1tonTest {
+    
+    public Weight_1tonTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getWeight method, of class Weight_1ton.
+     */
+    @Test
+    public void testGetWeight() {
+       
+        Weight_1ton instance = new Weight_1ton();
+        double expResult = 1.3;
+        double result = instance.getWeight();
+        assertEquals(expResult, result, 1.3);
+      
+    }
+    
+}
---
+++ Cardealership/test/cardealership/cars/Weight_2TonTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardealership.cars;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author silab
+ */
+public class Weight_2TonTest {
+    
+    public Weight_2TonTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getWeight method, of class Weight_2Ton.
+     */
+    @Test
+    public void testGetWeight() {
+      
+        Weight_2Ton instance = new Weight_2Ton();
+        double expResult = 2.6;
+        double result = instance.getWeight();
+        assertEquals(expResult, result, 2.6);
+       
+    }
+    
+}
