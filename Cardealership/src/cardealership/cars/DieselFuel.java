33272cad144db85bffb7381e1883f04413bccb30
Mxolisi Silabela <silabelamxolisi@gmail.com>
2020-06-24 16:03:17 +0200

    Fuel Type Interface and classes.

---
+++ Cardealership/src/cardealership/cars/DieselFuel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardealership.cars;
+
+/**
+ *
+ * @author silab
+ */
+public class DieselFuel implements IFuelType{
+
+    @Override
+    public String TypeOfFuel() {
+       return "Diesel";
+    }
+    
+}
